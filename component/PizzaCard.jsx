c28d6ff68c5d312c7a26313aea2afc65ab36c75f
hrithin220900 <65213514+hrithin220900@users.noreply.github.com>
2022-07-09 09:55:35 +0530

    first commit

---
+++ component/PizzaCard.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+import styles from "../styles/PizzaCard.module.css"
+import Link from "next/link"
+
+const PizzaCard = ({pizza}) => {
+  return (
+  <Link passHref href={`/product/${pizza._id}`}>
+    <div className={styles.container}>
+        <Image src={pizza.img} alt ="" width="500" height="500" />
+        <h1 className={styles.title}>{pizza.title}</h1>
+        <span className={styles.price}>${pizza.prices[0]}</span>
+        <p className={styles.desc}>
+          {pizza.desc}
+        </p>
+    </div>
+  </Link>
+  )
+}
+
+export default PizzaCard
